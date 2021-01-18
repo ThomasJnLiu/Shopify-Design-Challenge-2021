127a847bc09225b6f3c6c53526bb09dc07dc3187
Thomasjnliu <thomasjnliu@gmail.com>
2021-01-17 22:50:11 -0500

    Add project info

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,22 @@ const Header = () => {
     <div className="header">
       <img src={process.env.PUBLIC_URL + "/logo-small.png"} />
       <div className="header-text">
-        <h1>The Shoppies</h1>
-        <h2>Nominate your favourite movies!</h2>
+        <div>
+          <h1>The Shoppies</h1>
+          <h2>Nominate your favourite movies!</h2>
+        </div>
+
+        <div className="header-project-info">
+          <p>
+            Made by Thomas Liu for Shopify's Summer 2021 Intern Design Challenge
+          </p>
+          <a
+            target="_blank"
+            href="https://github.com/ThomasJnLiu/Shopify-Design-Challenge-2021"
+          >
+            Link to the repo
+          </a>
+        </div>
       </div>
     </div>
   );
